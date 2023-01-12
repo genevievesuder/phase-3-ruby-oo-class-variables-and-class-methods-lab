51ccaca8b5709546781f99675881f72d0c402c71
Genevieve Suder <genevievesuder@gmail.com>
2023-01-12 13:03:11 -0800

    commit tree abb6aabea29c24b8741cb108121c875ccce1d98b

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+require 'pry'
+
+class Song
+
+    attr_reader :name, :artist, :genre
+
+    @@artists = []
+    @@genres = []
+    @@count = 0
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    # def self.genre_count
+    #     genre_hash = {}
+    #     @@genres.each do |k| k.count
+    #     end
+    # end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
+# binding.pry
